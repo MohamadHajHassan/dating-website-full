9ed988f063aaa01d18a77b9453470d9f3fc75c31
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2022-10-05 13:41:33 +0300

    backend: fix fetch landing api

--- dating-server/app/Http/Controllers/LandingController.php
+++ dating-server/app/Http/Controllers/LandingController.php
@@ -3,14 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 
 class LandingController extends Controller
 {
-    function fetchLanding(Request $request){
-
-        $interest = $request -> input("interested_in");
-        $gender = $request -> input("gender");
+    function fetchLanding(){
+        $user = Auth::user();
+        $interest = $user -> interested_in;
+        $gender = $user -> gender;
         $users = User::where(["gender" => $interest, "interested_in" => $gender])->get();
 
         return response()->json([
